7877f017c4e5e5c2c68e97a5137da08b661b1249
gustavo3020 <gustavo-luigi@hotmail.com>
2025-08-22 23:19:45 -0300

    feat: Add initial models.

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    location_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class StockTransaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('IN', 'Stock In'),
+        ('OUT', 'Stock Out'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    transaction_type = models.CharField(max_length=3, choices=TRANSACTION_TYPES)
+    timestamp = models.DateTimeField(auto_now_add=True)
