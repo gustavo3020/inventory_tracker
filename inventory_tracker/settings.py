9671d5c3b1d95064154c686fd67985dcf375848f
gustavo3020 <gustavo-luigi@hotmail.com>
2025-08-22 22:25:57 -0300

    Configure database to use PostgreSQL.
    Updated the DATABASES setting to use PostgreSQL instead of SQLite3.

--- inventory_tracker/settings.py
+++ inventory_tracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'inventory_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventory_db',
+        'USER': 'postgres',
+        'PASSWORD': '=123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
