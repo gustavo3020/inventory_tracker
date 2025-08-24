b9130b6076d1b0c62b712da9b19579d498605ea8
gustavo3020 <gustavo-luigi@hotmail.com>
2025-08-23 22:49:43 -0300

    feat: Customize admin panel views

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Location, Product, StockTransaction
 
-admin.site.register(Location)
-admin.site.register(Product)
-admin.site.register(StockTransaction)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location_name', 'address')
+    search_fields = ('location_name', 'address')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'description')
+    search_fields = ('product_name', 'description')
+
+class StockTransactionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'location', 'transaction_type')
+    search_fields = ('product', 'location', 'transaction_type')
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(StockTransaction, StockTransactionAdmin)
